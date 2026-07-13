f97a01cce96be3d2e913fdb76db2fbd2098f2494
Erick Kanja <e.kanja@alustudent.com>
2026-07-13 19:09:55 +0200

    Creat profile and about pages

--- drawer/lib/main.dart
+++ drawer/lib/main.dart
@@ -52,100 +52,38 @@ class SettingsPage extends StatelessWidget {
   }
 }
 
-// class ProfilePage extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text("Profile"),
-//       ),
-//       drawer: buildDrawer(context),
-//       body: Center(
-//         child: Text(
-//           "This is Profile Page",
-//           style: TextStyle(fontSize: 22),
-//         ),
-//       ),
-//     );
-//   }
-// }
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Profile"),
+      ),
+      drawer: buildDrawer(context),
+      body: Center(
+        child: Text(
+          "This is Profile Page",
+          style: TextStyle(fontSize: 22),
+        ),
+      ),
+    );
+  }
+}
 
-// class AboutPage extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text("About"),
-//       ),
-//       drawer: buildDrawer(context),
-//       body: Center(
-//         child: Text(
-//           "This is About Page",
-//           style: TextStyle(fontSize: 22),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-// // Same drawer used on every page.
-// Widget buildDrawer(BuildContext context) {
-//   return Drawer(
-//     child: ListView( 
-//       padding: EdgeInsets.all(50.0),
-//       children: [
-//         DrawerHeader(
-//           decoration: BoxDecoration(color: Colors.red),
-//           child: Text(
-//             "Menu",
-//             style: TextStyle(color: Colors.white, fontSize: 24),
-//           ),
-//         ),
-//         ListTile(
-//           leading: Icon(Icons.home),
-//           title: Text("Home"),
-//           onTap: () {
-//             Navigator.pop(context); // close the drawer
-//             Navigator.pushReplacement(
-//               context,
-//               MaterialPageRoute(builder: (context) => HomePage()),
-//             );
-//           },
-//         ),
-//         ListTile(
-//           leading: Icon(Icons.settings),
-//           title: Text("Settings"),
-//           onTap: () {
-//             Navigator.pop(context);
-//             Navigator.pushReplacement(
-//               context,
-//               MaterialPageRoute(builder: (context) => SettingsPage()),
-//             );
-//           },
-//         ),
-//         ListTile(
-//           leading: Icon(Icons.person),
-//           title: Text("Profile"),
-//           onTap: () {
-//             Navigator.pop(context);
-//             Navigator.pushReplacement(
-//               context,
-//               MaterialPageRoute(builder: (context) => ProfilePage()),
-//             );
-//           },
-//         ),
-//         ListTile(
-//           leading: Icon(Icons.info),
-//           title: Text("About"),
-//           onTap: () {
-//             Navigator.pop(context);
-//             Navigator.pushReplacement(
-//               context,
-//               MaterialPageRoute(builder: (context) => AboutPage()),
-//             );
-//           },
-//         ),
-//       ],
-//     ),
-//   );
-// }
+class AboutPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("About"),
+      ),
+      drawer: buildDrawer(context),
+      body: Center(
+        child: Text(
+          "This is About Page",
+          style: TextStyle(fontSize: 22),
+        ),
+      ),
+    );
+  }
+}
